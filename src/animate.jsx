fea92f3cf7c2c0d6eb4c6713682d39e6079ff0aa
GovarthananD <govarthananduraisamy@gmail.com>
2025-10-16 01:31:48 +0530

    animate code updated

--- src/animate.jsx
+++ src/animate.jsx
@@ -3,17 +3,16 @@ import { animate } from "motion";
 
 export default function AnimatedHeading() {
   useEffect(() => {
-    animate(".heading", 
-  { scale: [0.4, 1], opacity: [0, 1] }, 
-  { ease: "easeInOut", duration: 1.2 }
-);
-
+    animate(
+      ".heading",
+      { scale: [0.4, 1], opacity: [0, 1] },
+      { ease: "easeInOut", duration: 1.2 }
+    );
   }, []);
 
   return (
-    <h1 className="heading text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-white">
-  GOVARTHANAN D
-</h1>
-
+    <span className="heading text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-white inline-block">
+      GOVARTHANAN D
+    </span>
   );
 }

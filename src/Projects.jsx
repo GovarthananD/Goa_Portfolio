e49ce3bfd9d5a0fb854eff40a2a758ae01a0bc22
GovarthananD <govarthananduraisamy@gmail.com>
2025-10-16 01:21:03 +0530

    project update

--- src/Projects.jsx
+++
@@ -1,9 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="min-h-screen flex justify-center items-center bg-indigo-900 text-white px-6">
-      <h2 className="text-5xl md:text-6xl font-bold text-center">
-        Projects
-      </h2>
-    </div>
-  );
-}
